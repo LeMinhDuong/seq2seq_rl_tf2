93f3e1c044191e6e745b8e47994504cf36747597
LeMinhDuong <leminhduongvn@gmail.com>
2021-03-26 16:35:21 +0700

    commit

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ FLAGS = flags.FLAGS
 class Seq2Seq(object):
     def main(self, unused_argv):
         print(unused_argv)
-        
-def main(unused_argv):
+
+def main(argv):
   seq2seq = Seq2Seq()
-  seq2seq.main(unused_argv)
+  seq2seq.main(argv)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(main)
